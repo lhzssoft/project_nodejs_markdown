0cfab610d3efe2f0695abe6e16d681401d0481d2
google <google@microsoft.com>
2017-12-12 15:00:50 +0800

    'dist'

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -23,10 +23,10 @@ const webpackConfig = merge(baseWebpackConfig, {
   devtool: config.build.productionSourceMap ? config.build.devtool : false,
   output: {
     path: config.build.assetsRoot,
-    filename: utils.assetsPath('js/[name].[chunkhash].js'),
-    chunkFilename: utils.assetsPath('js/[id].[chunkhash].js'),
-
-    library: 'foo-markdown',
+    filename: '[name].js',
+    // filename: utils.assetsPath('js/[name].[chunkhash].js'),
+    // chunkFilename: utils.assetsPath('js/[id].[chunkhash].js'),
+    library: 'fooMarkdown',
     libraryTarget: 'umd',
     umdNamedDefine: true
   },
